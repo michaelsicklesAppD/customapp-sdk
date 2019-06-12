1d26643294d871ef4db2c213cc4f3bc5aad0d2f7
Michael Sickles <michael.sickles@appdynamics>
2019-06-12 15:38:03 -0400

    Adding in bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import billoard from "billboard.js"
 import c3 from "c3"
 import d3 from "d3"
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 var jsrender = require('jsrender')(); 
 // window.jQuery = $;
 // window.$ = $;
--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,32 @@ const path = require('path')
 const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
-  entry: ['./src/main.js'],
-  module: {
-    rules: [{
-      test: require.resolve('jquery'),
-      use: [{
-        loader: 'expose-loader',
-        options: 'jQuery'
-      },{
-        loader: 'expose-loader',
-        options: '$'
-      }]
-    },{
-        test: require.resolve('c3'),
-        use: [{
-          loader: 'expose-loader',
-          options: 'c3'
+    entry: ['./src/main.js'],
+    module: {
+        rules: [{
+            test: require.resolve('jquery'),
+            use: [{
+                loader: 'expose-loader',
+                options: 'jQuery'
+            }, {
+                loader: 'expose-loader',
+                options: '$'
+            }]
+        }, {
+            test: require.resolve('c3'),
+            use: [{
+                loader: 'expose-loader',
+                options: 'c3'
+            }]
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
         }]
-      }]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
-  },
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
 
 }
