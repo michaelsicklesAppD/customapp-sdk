937011c33cb441087b35fb71a429d340568e4969
Michael Sickles <michael.sickles@appdynamics>
2019-06-12 15:32:14 -0400

    Working through all dependendices... jsrender added in

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import billoard from "billboard.js"
 import c3 from "c3"
 import d3 from "d3"
 import $ from 'jquery';
+var jsrender = require('jsrender')(); 
 // window.jQuery = $;
 // window.$ = $;
 // console.log(window.jQuery);
