84cbbb9f75bbe34991f4eb1dc66b9b36172e2972
Michael Sickles <michael.sickles@appdynamics>
2019-06-12 16:14:43 -0400

    Fixing datatables and adding in c3 css

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import billoard from "billboard.js"
 import c3 from "c3"
+import 'c3/c3.min.css'
 import d3 from "d3"
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'datatables/media/css/jquery.dataTables.css';
+import datatables from 'datatables'
 var jsrender = require('jsrender')(); 
-var datatables = require('datatables')();
+
 // window.jQuery = $;
 // window.$ = $;
 // console.log(window.jQuery);
